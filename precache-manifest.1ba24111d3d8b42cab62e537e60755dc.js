1ebf1bb9b3d5293eee018b88406f0140835e4375
Cole Lin <cole.lin@hubdoc.com>
2019-01-19 18:19:34 -0500

    Updates

---
+++ precache-manifest.1ba24111d3d8b42cab62e537e60755dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "983bcc3c32414bcef3f2",
+    "url": "/shopify-2019-spring-intern-web-enginnering-challenge/static/js/runtime~main.983bcc3c.js"
+  },
+  {
+    "revision": "23bfbdafa3d9fea85832",
+    "url": "/shopify-2019-spring-intern-web-enginnering-challenge/static/js/main.23bfbdaf.chunk.js"
+  },
+  {
+    "revision": "d9a75dce1bc3699cf325",
+    "url": "/shopify-2019-spring-intern-web-enginnering-challenge/static/js/1.d9a75dce.chunk.js"
+  },
+  {
+    "revision": "23bfbdafa3d9fea85832",
+    "url": "/shopify-2019-spring-intern-web-enginnering-challenge/static/css/main.7e25ce79.chunk.css"
+  },
+  {
+    "revision": "8944fe521bb99119eba4b1b1d77cc64b",
+    "url": "/shopify-2019-spring-intern-web-enginnering-challenge/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/shopify-2019-spring-intern-web-enginnering-challenge/precache-manifest.1ba24111d3d8b42cab62e537e60755dc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/shopify-2019-spring-intern-web-enginnering-challenge/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
